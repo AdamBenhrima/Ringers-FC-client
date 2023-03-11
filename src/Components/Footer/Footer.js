f14d70982c927b74e025f90b839c43035f567450
Adam Benhrima <adam.benhrima@hotmail.com>
2023-03-11 01:56:41 +0000

    adjust links in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,30 +3,31 @@ import facebook from "../../assets/icons/Icon-facebook.svg";
 import instagram from "../../assets/icons/Icon-instagram.svg";
 import twitter from "../../assets/icons/Icon-twitter.svg";
 import football from "../../assets/icons/football-icon.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <a href="https://www.instagram.com/">
+        <a href="https://www.instagram.com/" target="_blank">
           <img
             src={instagram}
             alt="instagram logo"
             className="footer__social"
           />
         </a>
-        <a href="https://www.facebook.com/">
+        <a href="https://www.facebook.com/" target="_blank">
           <img src={facebook} alt="facebook logo" className="footer__social" />
         </a>
-        <a href="https://twitter.com/">
+        <a href="https://twitter.com/" target="_blank">
           <img src={twitter} alt="twitter logo" className="footer__social" />
         </a>
       </div>
       <div className="footer__container">
         <img src={football} alt="" className="footer__icon" />
-        <a href="/" className="footer__logo">
+        <Link to="/" className="footer__logo">
           RINGERS FC
-        </a>
+        </Link>
       </div>
     </footer>
   );
